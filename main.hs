1bf39f5588e0137ef80103658a8688246ae89f4f
Lev135 <lev_135@mail.ru>
2022-01-26 09:56:05 +0300

    Main program updated

--- main.hs
+++ main.hs
@@ -1,16 +1,33 @@
 module Main where
 import Control.Monad.Trans.Except (runExcept)
 import TestAlign (readDescr)
+import TestParser (makeParser)
+import Utils (parseAll)
 
+main :: IO ()
 main = do
-    putStr "Type input file name: "
-    fileName <- getLine
-    descr <- readFile ("descr/" ++ fileName ++ ".ttempl")
+    putStr "Type description file name: "
+    descrFileName <- getLine
+    descr <- readFile ("descr/" ++ descrFileName ++ ".ttempl")
     putStrLn "Input file code:"
     putStr descr
     putStrLn "Processed data:"
-    let outp = readDescr descr
-    case runExcept outp of
+    let descr' = readDescr descr
+    case runExcept descr' of
         Left  e      -> putStrLn e
-        Right descrs -> putStrLn $ concatMap ((++"\n") . show) descrs
-    
+        Right descrs -> do
+            putStrLn $ concatMap ((++"\n") . show) descrs
+            putStr "Test file name: "
+            testFileName <- getLine
+            test <- readFile ("test/" ++ testFileName ++ ".txt")
+            putStrLn "Test file code:"
+            putStr test
+            putStrLn "Processed test file:"
+            let p = makeParser descrs
+            case parseAll p test of
+                Left  e     -> print e
+                Right tmp   -> mapM_ h tmp
+    where
+        h ((name, ixs), v) = putStrLn $ name 
+                            ++ concatMap (\i -> "[" ++ show i ++ "]") ixs
+                            ++ " = " ++ show v
